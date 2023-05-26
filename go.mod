6bdc5f29c18bd173e6a0efafc68048f41401f11c
rubi <naiman.music.info@gmail.com>
2023-05-26 17:10:21 +0200

    random wallpapers

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/menaruben/RandomWallpapers
+
+go 1.19
+
+require (
+	github.com/reujab/wallpaper v0.0.0-20210630195606-5f9f655b3740 // indirect
+	golang.org/x/sys v0.0.0-20190215142949-d0b11bdaac8a // indirect
+	gopkg.in/ini.v1 v1.62.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ randwallpaper.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/reujab/wallpaper"
+)
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func getRandomImage(path string) (string, error) {
+	images, err := os.ReadDir(path)
+	if err != nil {
+		return "", err
+	}
+
+	// Initialize the random number generator with a different seed value each "time"
+	rand.Seed(time.Now().UnixNano())
+
+	randomIndex := rand.Intn(len(images))
+	randomImagePath := path + "/" + images[randomIndex].Name()
+
+	return randomImagePath, err
+}
+
+func setWallpaperLoop(path string, interval time.Duration, crop bool) {
+	for {
+		randomImagePath, err := getRandomImage(path)
+		check(err)
+
+		err = wallpaper.SetFromFile(randomImagePath)
+		check(err)
+
+		if crop {
+			err = wallpaper.SetMode(wallpaper.Crop)
+			check(err)
+		}
+		time.Sleep(interval)
+	}
+}
+
+func main() {
+	path := flag.String("path", "C:/repos/RandomWallpapers/images", "Path to the image directory")
+	interval := flag.Int("interval", 5, "Interval in seconds")
+	crop := flag.Bool("crop", true, "set mode of wallpapers to cropped")
+	flag.Usage = func() {
+		fmt.Fprintln(flag.CommandLine.Output(), "Options:")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	intervalDuration := time.Duration(*interval) * time.Second
+	setWallpaperLoop(*path, intervalDuration, *crop)
+}
